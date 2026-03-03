df69ffe52562bd5761195781b155ed7fde3037fc
AmazinAxel <axel@amazinaxel.com>
2026-03-03 14:28:17 -0800

    feat: add tailwind!

--- app/pages/clickToOpen.vue
+++ app/pages/clickToOpen.vue
@@ -1,5 +1,7 @@
 <template>
-    <div @click="navigateTo('/book')">
+    <div @click="navigateTo('/book')"
+        class="flex items-center justify-center h-screen"
+    >
         <h1>Nix my beloved</h1>
         <h2>(click to continue)</h2>
         <!-- images everywhere of nix flakes moving around and dancing, arrow to right with blue card and white background -->
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+
   app: {
     head: {
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
     }
-  }
+  },
+
+  modules: ['@nuxtjs/tailwindcss']
 })
