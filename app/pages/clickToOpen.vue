35cd26f66040d70d58f2877275458d00a06b16a4
AmazinAxel <axel@amazinaxel.com>
2026-03-03 15:08:31 -0800

    feat: custom tailwind colors

--- app/pages/clickToOpen.vue
+++ app/pages/clickToOpen.vue
@@ -2,8 +2,12 @@
     <div @click="navigateTo('/book')"
         class="flex items-center justify-center h-screen"
     >
-        <h1>Nix my beloved</h1>
-        <h2>(click to continue)</h2>
-        <!-- images everywhere of nix flakes moving around and dancing, arrow to right with blue card and white background -->
+        <div @click="navigateTo('/book')"
+            class="bg-blue-2 p-5"
+        >
+            <h1 class="text-dark-1">Nix my beloved</h1>
+            <h2>(click to continue)</h2>
+            <!-- images everywhere of nix flakes moving around and dancing, arrow to right with blue card and white background -->
+        </div>
     </div>
 </template>
