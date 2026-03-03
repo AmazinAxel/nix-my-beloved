d9343829f05da758da699bbf7862d552f7a755b2
AmazinAxel <axel@amazinaxel.com>
2026-03-03 15:26:47 -0800

    feat: heroicons

--- app/pages/clickToOpen.vue
+++ app/pages/clickToOpen.vue
@@ -1,13 +1,20 @@
+<script setup>
+    import { ArrowRightIcon } from '@heroicons/vue/24/solid'
+</script>
+
 <template>
     <div @click="navigateTo('/book')"
         class="flex items-center justify-center h-screen"
     >
-        <div @click="navigateTo('/book')"
-            class="bg-blue-2 p-5"
+        <button @click="navigateTo('/book')"
+            class="bg-blue-2 p-5 rounded-md flex gap-3 vertical-align"
         >
-            <h1 class="text-dark-1">Nix my beloved</h1>
-            <h2>(click to continue)</h2>
-            <!-- images everywhere of nix flakes moving around and dancing, arrow to right with blue card and white background -->
-        </div>
+            <div class="flex-col vertical-align">
+                <h1 class="text-light-1 text-xl">Nix my beloved</h1>
+                <h2 class="text-light-3 text-lg">(click to continue)</h2>
+            </div>
+            <ArrowRightIcon class="h-16 w-16 text-light-2 vertical-align"/>
+        </button>
+        <!-- images everywhere of nix flakes moving around and dancing, arrow to right with blue card and white background -->
     </div>
 </template>
