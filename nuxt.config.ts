3d585cc9bfa010d823fa0bb35a8ed48572046dc7
AmazinAxel <axel@amazinaxel.com>
2026-03-02 18:25:47 -0800

    feat: actually make favicon work

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
+    }
+  }
 })
