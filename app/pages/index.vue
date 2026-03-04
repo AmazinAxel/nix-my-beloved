5ead9bca319f1154afa556a0b08c1bd9c8b6f817
AmazinAxel <axel@amazinaxel.com>
2026-03-03 21:53:54 -0800

    fix: move path

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,20 @@
 <script setup>
-    await navigateTo('/clickToOpen'); // This is where the app starts
-
-    /* maybe include some logic or something here idk */
+    import { ArrowRightIcon } from '@heroicons/vue/24/solid'
 </script>
+
+<template>
+    <div @click="navigateTo('/book')"
+        class="flex items-center justify-center h-screen"
+    >
+        <button @click="navigateTo('/book')"
+            class="bg-blue-2 p-5 rounded-md flex gap-3 vertical-align"
+        >
+            <div class="flex-col vertical-align">
+                <h1 class="text-light-1 text-xl">Nix my beloved</h1>
+                <h2 class="text-light-3 text-lg">(click to continue)</h2>
+            </div>
+            <ArrowRightIcon class="h-16 w-16 text-light-2 vertical-align"/>
+        </button>
+        <!-- images everywhere of nix flakes moving around and dancing, arrow to right with blue card and white background -->
+    </div>
+</template>
