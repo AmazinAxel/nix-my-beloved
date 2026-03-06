b7b2504c439897573622ff987bea7b2ce19c292b
AmazinAxel <axel@amazinaxel.com>
2026-03-06 11:19:31 -0800

    feat: make particles look good

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,6 +8,14 @@
     const particles = (loc, amount) => {
         confetti({
             particleCount: amount,
+            spread: 360,
+            startVelocity: 5,
+            flat: true, // looks better
+            decay: 0.88, // how fast speed changes
+            gravity: 0.1,
+            ticks: 25,
+            shapes: ['square'],
+            colors: ['#5e81ac', '#81a1c1'],
             origin: {
                 x: loc.clientX / window.innerWidth,
                 y: loc.clientY / window.innerHeight
@@ -21,7 +29,10 @@
     <BookView v-if="open"/>
     <div v-else class="flex items-center justify-center h-screen bg-light-3 z-[10]" >
         <button
-            @click="open = true"
+            @click="(event) => {
+                buttonClickParticles(event, 20); // todo this looks bad
+                open = true;
+            }"
             class="group bg-blue-2 p-5 rounded-md flex gap-3 content-center w-[17rem]
 
             transition-all duration-50 ease-out
@@ -50,14 +61,22 @@
                 rotate: Math.random() * 100 + 'deg'
             }"
 
-            @mouseenter="particles($event, 10, 100)"
-            @click="particles($event, 10, 1000)"
+            @mouseenter="particles($event, 2)"
+            @mousedown="(event) => {
+                particles(event, 5)
+                
+
+                // i cant overwrite the other styles so we have to apply them manually unfortunately
+                event.target.style.opacity = '0'
+                event.target.style.transform = 'scale(-1) rotate(0deg)'
+                event.target.style.pointerEvents = 'none'
+                //setTimeout(() => event.target.remove(), 500)
+            }"
 
             class="w-10 z-[0] absolute opacity-50
 
                 transition-all duration-[1000ms] ease-out
                 hover:rotate-[300deg] hover:scale-[3]
-                active:scale-[1.2] active:rotate-[0deg]
             "/>
         </div>
     </div>
