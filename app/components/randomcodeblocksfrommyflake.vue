1ef79836de0c5ef71a11fa96d507d79ff91b7807
AmazinAxel <axel@amazinaxel.com>
2026-03-09 12:25:14 -0700

    chore: improve script for getting expressions

--- app/components/randomcodeblocksfrommyflake.vue
+++ app/components/randomcodeblocksfrommyflake.vue
@@ -1,1 +1,10 @@
-<!-- todo -->
+<!-- todo
+ /* how this is gonna work
+the randomcodeblocks code will grab a random element from this json and display it in the background scrolling and fading
+might want to grab a random section from the expression since sometimes it can be long
+*/
+ -->
+
+<template>
+    
+</template>
