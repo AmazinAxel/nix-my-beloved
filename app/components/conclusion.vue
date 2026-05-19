272cff4c9fe9b31c2350b8802b9f6197b2ff5b6e
AmazinAxel <axel@amazinaxel.com>
2026-05-19 07:14:29 -0700

    chore: update conclusion page

--- app/components/conclusion.vue
+++ app/components/conclusion.vue
@@ -3,7 +3,6 @@
         <h1 class="text-[2rem] font-bold font-header leading-tight text-dark-4">thank u, Nix</h1>
         <p class="gap-1 text-[1rem] italic font-body opacity-75 leading-tight text-dark-3 pb-[5px]">spending 180+ hours and the past 2.5 years of my life is something i shall <span class="line-through">never</span> regret</p>
         <p class="gap-1 text-[0.9rem] italic font-body opacity-60 leading-tight text-dark-3 pb-[5px]">(background is my Nix code from <a href="https://github.com/amazinaxel/flake" class="text-blue-1 underline">my flake</a> btw)</p>
-        <p class="text-[0.7rem] italic font-body opacity-25 leading-tight text-dark-3">OH and if anyone reading this lives in the PNW you should come to my talk about declarative systems with Nix & NixOS at <a class="text-blue-1 underline" href="https://linuxfestnorthwest.org/">LFNW26</a> @ Bellingham Tech this April! 💖</p>
         <p class="text-[0.7rem] italic font-body opacity-25 leading-tight text-dark-3 mt-[10px] mb-[10px]">tanks for reading bye</p>
     </Page>
 </template>
